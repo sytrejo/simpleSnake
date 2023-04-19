7fa3018a7917254fd1371b96aeed6e19ce77e75b
sytrejo <s.y.trejo26@gmail.com>
2023-04-19 16:50:30 -0400

    starting the program

---
+++ script.js
@@ -0,0 +1,4 @@
+const snakeboard= document.getElementById("gameCanvas");
+const snakeboard_ctx= gameCanvas.getContext("2d");
+
+let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
